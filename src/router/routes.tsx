1d870918eba64d15bb4c6b79fbe01b1b5ab5183b
Pradip Bhusnar <bhusnarpradip@gmail.com>
2024-05-22 10:21:43 +0530

    feat: updated routes

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import type { PathRouteProps } from 'react-router-dom';
 
+import { Login } from '@/pages/login/login';
 import TodoBoardDashboard from '@/pages/todoBoardDashboard';
 
 const Home = React.lazy(() => import('@/pages/home'));
@@ -9,6 +10,10 @@ export const routes: Array<PathRouteProps> = [
   {
     path: '/',
     element: <Home />,
+  },
+  {
+    path: '/login',
+    element: <Login />,
   },
   {
     path: '/todo-board',
